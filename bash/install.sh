5d2d5add4f1e0255b2169e9195b38c23d221376b
Enri2077 <enry2077@gmail.com>
2017-03-08 10:00:53 +0100

    Corrected spelling

--- bash/install.sh
+++ bash/install.sh
@@ -1,6 +1,6 @@
 UDEV_RULE_FILE=/etc/udev/rules.d/99-crystalfontz.rules
 if [ -e $UDEV_RULE_FILE ];
-	then echo "The file "$UDEV_RULE_FILE" alrteady exists; Doing nothing.";
+	then echo "The file "$UDEV_RULE_FILE" already exists; Doing nothing.";
 	else
 		echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="fc0d", SYMLINK+="crystalfontz", MODE="0666"' >> $UDEV_RULE_FILE;
 		echo "Created file $UDEV_RULE_FILE."
